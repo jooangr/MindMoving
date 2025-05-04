70165feefca9b8ae0ef7c21005707e716ee453db
Joan <joangallardorobles@gmail.com>
2025-05-04 13:00:49 +0200

    Add topBar

--- app/src/main/java/com/example/mindmoving/views/menu/MainScreenMenu.kt
+++ app/src/main/java/com/example/mindmoving/views/menu/MainScreenMenu.kt
@@ -3,33 +3,45 @@ package com.example.mindmoving.views.menu
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.mindmoving.navigation.TopNavigationBar
 
 @Composable
 fun MenuPantallaPrincipal(navController: NavHostController) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-
-        //Esta provisional esto, aqui habra algo mejor y bonito pero es solo prueba
-        Text("Menú Principal")
-        Button(onClick = { navController.navigate("atencion") }) {
-            Text("Comprobar nivel de Atencion")
+    Scaffold(
+        topBar = {
+            TopNavigationBar(title = "MindMoving")
         }
-        //TODO cambiar el uso de botones ya que esto no sera asi
-        Button(onClick = { navController.navigate("parpadeo") }) {
-            Text("Controlar el coche")
-        }
-        //TODO cambiar el uso de botones ya que esto no sera asi
-        Button(onClick = { navController.navigate("meditacion") }) {
-            Text("Ajustar Conecentracion")
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+                .padding(16.dp), // Padding extra si deseas
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            //Esta provisional esto, aqui habra algo mejor y bonito pero es solo prueba
+            Text("Menú Principal")
+            Button(onClick = { navController.navigate("atencion") }) {
+                Text("Comprobar nivel de Atencion")
+            }
+            //TODO cambiar el uso de botones ya que esto no sera asi
+            Button(onClick = { navController.navigate("parpadeo") }) {
+                Text("Controlar el coche")
+            }
+            //TODO cambiar el uso de botones ya que esto no sera asi
+            Button(onClick = { navController.navigate("meditacion") }) {
+                Text("Ajustar Conecentracion")
+            }
         }
     }
 }
