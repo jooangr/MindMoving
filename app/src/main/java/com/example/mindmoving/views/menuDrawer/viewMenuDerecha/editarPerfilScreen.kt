b60fd42d72668b930643ae54d6d2b41c12b997cc
Joan <joangallardorobles@gmail.com>
2025-05-27 19:43:40 +0200

    View editPerfilUser funcionando la interfaz correctamemte, falter porbar el backend

--- app/src/main/java/com/example/mindmoving/views/menuDrawer/viewMenuDerecha/editarPerfilScreen.kt
+++ app/src/main/java/com/example/mindmoving/views/menuDrawer/viewMenuDerecha/editarPerfilScreen.kt
@@ -19,9 +19,11 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import com.example.mindmoving.retrofit.ApiClient
 import com.example.mindmoving.retrofit.models.ActualizarUsuarioRequest
+import com.example.mindmoving.retrofit.models.UsuarioResponse
 import com.example.mindmoving.retrofit.models.VerificarPasswordRequest
 import kotlinx.coroutines.launch
 
@@ -43,16 +45,26 @@ fun EditarPerfilScreen(navController: NavHostController) {
     val prefs = context.getSharedPreferences("prefs", Context.MODE_PRIVATE)
     val userId = prefs.getString("userId", null)
 
-    val fondoGradiente = Brush.verticalGradient(colors = listOf(Color(0xFF8E2DE2), Color(0xFF4A00E0)))
+
+    //Guardar varibales locales
+    var userActual by remember { mutableStateOf<UsuarioResponse?>(null) }
+    var emailActual by remember { mutableStateOf<UsuarioResponse?>(null) }
+
+
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(fondoGradiente)
+            .background(
+                Brush.verticalGradient(
+                    listOf(Color(0xFF3F51B5), Color(0xFFB0C4DE))
+                )
+            )
             .padding(24.dp),
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+        Spacer(modifier = Modifier.height(32.dp))
         Text("Editar Perfil", style = MaterialTheme.typography.headlineSmall, color = Color.White)
 
         if (!verificado) {
@@ -78,6 +90,8 @@ fun EditarPerfilScreen(navController: NavHostController) {
                             val userResponse = apiService.getUsuario(userId ?: "")
                             if (userResponse.isSuccessful) {
                                 val user = userResponse.body()
+                                userActual = user
+                                emailActual = (user?.email ?: "") as UsuarioResponse?
                                 usernameNuevo = user?.username ?: ""
                                 emailNuevo = user?.email ?: ""
                             }
@@ -93,24 +107,48 @@ fun EditarPerfilScreen(navController: NavHostController) {
             }
 
         } else {
-            OutlinedTextField(
-                value = usernameNuevo,
-                onValueChange = { usernameNuevo = it },
-                label = { Text("Nuevo username") },
-                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
-            )
+            Spacer(modifier = Modifier.height(35.dp))
+            // Usuario
+            Column(modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)) {
+                Text(
+                    text = "Nuevo username (Actual: ${userActual?.username ?: "..."})",
+                    style = MaterialTheme.typography.labelSmall,
+                    color = Color.White,
+                    fontSize = 14.sp,
+                    modifier = Modifier.padding(start = 8.dp, bottom = 2.dp)
+                )
 
-            OutlinedTextField(
-                value = emailNuevo,
-                onValueChange = { emailNuevo = it },
-                label = { Text("Nuevo email") },
-                modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)
-            )
+                OutlinedTextField(
+                    value = usernameNuevo,
+                    onValueChange = { usernameNuevo = it },
+                    placeholder = { Text("Escribe tu nuevo username") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            Spacer(modifier = Modifier.height(29.dp))
+
+// Email
+            Column(modifier = Modifier.fillMaxWidth().padding(vertical = 4.dp)) {
+                Text(
+                    text = "Nuevo email (Actual: ${userActual?.email ?: "..."})",
+                    style = MaterialTheme.typography.labelSmall,
+                    color = Color.White,
+                    fontSize = 14.sp,
+                    modifier = Modifier.padding(start = 8.dp, bottom = 2.dp)
+                )
+                OutlinedTextField(
+                    value = emailNuevo,
+                    onValueChange = { emailNuevo = it },
+                    placeholder = { Text("Escribe tu nuevo email") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+            Spacer(modifier = Modifier.height(29.dp))
 
             OutlinedTextField(
                 value = passwordNueva,
                 onValueChange = { passwordNueva = it },
-                label = { Text("Nueva contraseña") },
+                label = { Text("Nueva contraseña)") },
                 visualTransformation = if (mostrarPassword) VisualTransformation.None else PasswordVisualTransformation(),
                 trailingIcon = {
                     val icono = if (mostrarPassword) Icons.Filled.VisibilityOff else Icons.Filled.Visibility
